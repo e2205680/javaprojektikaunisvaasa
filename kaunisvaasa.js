43954768e85c277c8acfc8b7d9137f62903317b9
e2205680 <ivonne.cuadrosevangelista@gmail.com>
2024-12-30 06:40:50 +0200

    many changes

--- kaunisvaasa.js
+++ kaunisvaasa.js
@@ -4,7 +4,7 @@ let pic = document.getElementById("kuva0");
 
 let kuvat = [];
 for (let index = 0; index <21; index++) {
-    kuvat[index] = "img_"+index;
+    kuvat[index] = "img_"+index+".jpg";
 }
 
 console.log("Array of kuvat names:");
@@ -16,7 +16,7 @@ let numero=1;
 function omaFunktio() {
 
     console.log(kuvat[numero]);
-    pic.src = kuvat[numero]+".jpg";
+    pic.src = kuvat[numero];
     
     numero=numero+1;
     if(numero==kuvatLength) {
--- pelikone.js
+++ pelikone.js
@@ -1,57 +1,57 @@
-//let pic = document.getElementById("kuva0");
+window.onload=inicio;
 
-let imagenes = [];
-for (let index = 0; index <21; index++) {
-    imagenes[index] = "img_"+index+".jpg";
-} 
+// Se usa el array kuvat para las imagenes
 
-let premios =[]; // Mensajes numerados deacuerdo a la numeración de las pics
+let premios =["amor", "salud", "dinero", "amistad", "viajes"]; // Mensajes deacuerdo a la numeración de las pics
 
 let numeros_actuales=[];
 
+
 function inicio(){
     document.getElementById("tirar").onclick=lanzar_inicio;
-
 }
 
 function lanzar_inicio(){
 
     numeros_actuales=[];
 
-    for (let k=0; k<document.getElemenstByClassName("ventana").length; k++){
+    let maximo = document.getElementsByClassName("ventana").length;
+
+    for (let k=0;k<maximo;k++){
         numeros_actuales.push(escoger_numero());
         mostrar_imagen(k,numeros_actuales[k]);
     }
     console.log(numeros_actuales);
+
+    comparar();
 }
 
 function escoger_numero(){
-    let azar=Math.floor(Math.random() * imagenes.length);
+    let azar=Math.floor(Math.random() * 3);  //kuvat.lenght=3
     return azar;
 }
 
-function mostrar_imagen(num, im){
-    document.getElementsByClassName("imagen")[num,im].getElementsByTagName("img")[0].src=+imagenes[im];
+function mostrar_imagen(num,im){
 
-
+    document.getElementsByClassName("imagen")[num].getElementsByTagName("img")[0].src=kuvat[im]; 
 }
 
 function comparar(){
 
+   const mensaje = document.getElementById("mensaje");
+
+   if(numeros_actuales[0]==numeros_actuales[1] && numeros_actuales[1]==numeros_actuales[2]){
+                
+        mensaje.innerText = premios[numeros_actuales[0]];
+        console.log("Mensaje ganador: "+numeros_actuales[0]);
+
+        play();  // activa el audio
+   }
 }
 
-function actualizar(){
 
-}
+function play(){
 
-function mostrar_mensaje(){
-
-}
-
-function cerrar(){
-
-}
-
-function sonar(){
-
+    let audio = new Audio("http://codeskulptor-demos.commondatastorage.googleapis.com/descent/gotitem.mp3");
+    audio.play();      
 }
