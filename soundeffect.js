f74322b47eb8493879a2f146d5f9600168adca2e
e2205680 <ivonne.cuadrosevangelista@gmail.com>
2025-01-01 22:51:57 +0200

    Adding file soundeffect.js

---
+++ soundeffect.js
@@ -0,0 +1,11 @@
+// window.onload = aloittaa;
+
+function soundVoittaja(){
+    let audioVoittaja = new Audio("https://codeskulptor-demos.commondatastorage.googleapis.com/descent/gotitem.mp3");
+    audioVoittaja.play();     
+}
+
+function playGameLevel(){
+    let audioGameLevel = new Audio("https://codeskulptor-demos.commondatastorage.googleapis.com/pang/pop.mp3");
+    audioGameLevel.play();   
+}
