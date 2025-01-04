8195f440bf124a105e5ef5b8e92d7c0a53645dea
e2205680 <ivonne.cuadrosevangelista@gmail.com>
2025-01-04 12:19:21 +0200

    Adding comment parte 2: index, kaunissvaasa, pelikone

--- kaunisvaasa.js
+++ kaunisvaasa.js
@@ -1,77 +1,93 @@
-// Writing comments part 1
 // Showing in the console the title of the proyect 
 console.log("Kaunis Vaasa - Finland"); 
 
-//Using "document.getElementById" property to ...
+//Using ".getElementById" method to get the following buttons from the html document.
 const buttonEdellinen = document.getElementById("buttonEdellinen");
 const buttonSeurava = document.getElementById("buttonSeurava");
 
-// Using the "addEventListener" property to ...
+/* Using the ".addEventListener" method to execute 
+   the "muutaKuvaSound" function when you click on the specific button. */
 buttonEdellinen.addEventListener("click", muutaKuvaSound);
 buttonSeurava.addEventListener("click", muutaKuvaSound);
 
-// Using the "addEventListener" property to ...
+/* Using the "addEventListener" method to execute
+   the "nappiEdellinenKuva" function when you click on the specific button */
 buttonEdellinen.addEventListener("click", nappiEdellinenKuva);
 buttonSeurava.addEventListener("click", nappiSeuravaaKuva);
 
-// Using "document.getElementById" property to ...
+// Using ".getElementById" method to get the image with id="kuva0".
 let pic = document.getElementById("kuva0");
 
-//Creating Array "kuvat" to ...
+// Creating an empty Array "kuvat" to place the names of the pictures (21 pictures)
 let kuvat = [];
-//Using "for loop" to ...
+
+/* 
+1.- Using "for loop" statement to iterate from 0 
+    to a number lower than 21 (total amount of pictures available).
+2.- The "for loop" statement let me write the names of the pictures in an automatic way 
+    when the names have a consecutive numeration. 
+3.- Creating the "index" variable to keep the index values of each iteration.  
+  */
 for (let index = 0; index <21; index++) {
-    // Using "push" method to ...
+
+    /* 
+    Using the ".push" method to add the names 
+    created for of each picture in the Array "kuvat". 
+    */
     kuvat.push("img_"+index+".jpg");
 }
 // Showing values of Array "kuvat" in the console
 console.log("Array of kuvat names:");
 console.log(kuvat);
 
-// Using ".length" property to ...
+// Using ".length" property to count the elements in the Array "kuvat".
 let kuvatLength = kuvat.length;
-// Creating the index "numero"
+
+// Creating variable "numero" as index to Array "kuvat". 
 let numero = 0; 
 
-// Creating "katsoKuva" function to ...
+// Creating "katsoKuva" function to show the picture in the webpage according to the index.
 function katsoKuva(){
     // Showing in the console the name of the kuva with the index "numero"
     console.log(kuvat[numero]);
-    // Using ".src" property to ...
+
+    /* Using the ".src" property to replace the element 
+    of id = "kuva0" in the Album (img_0)
+    with the new picture "kuvat" of index "numero".  */
     pic.src = kuvat[numero];
 }
 
-// Creating "nappiSeuravaaKuva" function to ...
+// Creating "nappiSeuravaaKuva" function to go forward in the Album.
 function nappiSeuravaaKuva() {
-    // Incrementing by 1 the value of the index "numero"
+
+    // Increasing by 1 the value of the index "numero".
     numero = numero +  1;
-    /*
+    /* Using If statement:
     Condition: If the index "numero" is equal or more than the amount 
-               of elements "kuvaLength",
-               the index "numero" receives the numerical value: 0
+               of elements "kuvaLength", then
+               the index "numero" receives the numerical value: 0 .
      */
     if(numero >= kuvatLength) {
         numero = 0;  
     }
-    // Calling function "katsoKuva"
+    // Calling function "katsoKuva" to update the picture with the new index.
     katsoKuva();
 }
 
-// Creating "nappiEdellinenKuva" function to ...
+// Creating "nappiEdellinenKuva" function to go back in the Album.
 function nappiEdellinenKuva() {
-    // Decrementing by 1 the value of the index "numero"
+
+    // Decreasing by 1 the value of the index "numero".
     numero = numero - 1;
-    /*
+    /* Using If statement:
     Condition: If the index "numero" is less than the amount 
-               of elements "kuvaLenght",
+               of elements "kuvaLenght", then
                the index "numero" receives the numerical value
                of kuvaLength minus 1
      */
     if(numero < 0) {
         numero = kuvatLength - 1;  
     }    
-    // Calling function "katsoKuva"
-    katsoKuva();
-    
-
+    // Calling function "katsoKuva" to update the picture with the new index.
+    katsoKuva();   
 }
--- pelikone.js
+++ pelikone.js
@@ -1,29 +1,33 @@
-// Writing comments part 1
-// Using "window.onload" property to ...
-window.onload = aloittaa;
+/* Using "window.onload" property to load the webpage
+   before executing any function.  */
 
-// Käyttämällä kuvat array - from kaunisvaasa.js 
+/* The  "window.onload = aloittaa" means that the function "aloitta"
+   will be executed after the webpage has been loaded. */
+window.onload = aloittaa; 
 
-// Using "document.getElementById" property to ...
+// Käyttämällä kuvat array "from kaunisvaasa.js" file.
+
+/* Using "document.getElementById" method to get 
+   the following buttons from the html document. */
 const buttonLevelEasy = document.getElementById("easy");
 const buttonLevelMedium = document.getElementById("medium");
 const buttonLevelHard = document.getElementById("hard");
 
-// Using ".addEventListener" property to ...
+/* Using the ".addEventListener" method to execute 
+   the "painaNappiSound" function when you click on the specific button.  */
 buttonLevelEasy.addEventListener("click", painaNappiSound);
 buttonLevelMedium.addEventListener("click", painaNappiSound);
 buttonLevelHard.addEventListener("click", painaNappiSound);
 
-// Using "document.getElementsByClassName" property to ...
+/* Using "document.getElementsByClassName" method 
+   to get the elements with the tag "ikkuna" and "imagen"  */
 const ikkunaLaskuri = document.getElementsByClassName("ikkuna");
 const imagenLaskuri = document.getElementsByClassName("imagen");
 
-// viesti jokaiselle kuvalle - yhteensä 21 viestiä
+// Viesti jokaiselle kuvalle - yhteensä 21 viestiä.
 
-/*
-Creating the Array "palkinot" which elements are the messages
-that the user will get as prize when makes a match of 3 pictures
-*/
+/* Creating the Array "palkinot" which elements are the messages
+   that the user will get as award when makes a match of 3 pictures.  */
 let palkinnot =[
     "Unelmat eivät ole liian suuria, ne vain odottavat, että rohkeutesi kasvaa tarpeeksi tavoittaaksesi ne",
     "Jokainen päivä on uusi mahdollisuus aloittaa alusta ja kirjoittaa oma tarinasi.",
@@ -48,98 +52,137 @@ let palkinnot =[
     "Kaikki mitä tarvitset, on jo sisälläsi – odottamassa, että vapautat sen."
 ]; 
 
-// Creating "aloittaa" function to ..
+// Creating "aloittaa" function to set up event listeners to the specific buttons.
 function aloittaa(){   
 
-    // Using ".addEventListener" property to ...
+    /* Using the ".addEventListener" method to execute 
+       the "painaNappi" function when you click on the specific button. */
     buttonLevelEasy.addEventListener("click", painaNappi); 
     buttonLevelMedium.addEventListener("click", painaNappi);
     buttonLevelHard.addEventListener("click", painaNappi);      
 }
 
-// Creating "paikka" variable to ...
+// Creating "paikka" variable where will be the images (3 images) for game Pelikone.
 let paikka;
 
-// Creating "painaNappi" function to ...
+/* Creating "painaNappi" function to settle which picture
+   and in what place (ikkuna) will be shown the pictures. */ 
 function painaNappi(){
 
-    // Giving to "paikka" the value of an empty Array
+    // Giving to "paikka" the value of an empty Array.
     paikka=[];
 
-    // Using ".length" property to ...
-    let luku = ikkunaLaskuri.length;
+/*  1.- Using ".length" property to count the elements with class name "ikkuna".
+    2.- Creating variable "luku" to keep the value of "ikkunaLaskuri.length".  */
+    let luku = ikkunaLaskuri.length; // ikkunaLaskuri.length = 3..
 
-    // Using "for loop" to ...
-    // Creating the index "arvo"
+    /* Using "for loop" statemet to iterate from 0 to an integer lower than "luku" value.
+       Creating the index "arvo" to keep the counting in the iteration. */
     for (let arvo=0; arvo<luku; arvo++){
 
-        /*Calling the "valitseKuva" function 
-        inside the push method
-        */
-        // Using the ".push" method to ...
+        // Calling the "valitseKuva" function inside the push method.
+        
+        /* Using the ".push" method to add the numerical values, provided
+           by "valitseKuva function" (21 values max) to the Array "paikka". */
         paikka.push(valitseKuva());
-        // Calling "laittaKuva" function to ...
+        /* Calling "laittaKuva" function to show the image from the Array "kuvat"
+           that was created in the file "kaunisvaasa.js". */
         laittaaKuva(arvo,paikka[arvo]);
+
+    /* How it works: 
+    1.- The "for loop" makes that in each "arvo" value, 
+    functions valitseKuva and laittaKuva are called, example: arvo = 2
+    2.- valitseKuva() returns a number between 0 and 21,
+    depending of the level choosen, example:  8 when chose level medium (button 2).
+    3.- laittaKuva() requires 2 parameters, example: arvo = 2; paikka[2] = 8 ,
+    It means that picture "img_8.jpg" will be placed in the ikkuna "2".  */
     }
+    
     // Showing the Array "paikka" in the console
     console.log(paikka);
 
-    // Calling function "vertailla" to ...
+    /* Calling function "vertailla" to select the message 
+       when the user make a match of 3 pictures. */
     vertailla();
 }
 
 // Adding code for the 3 buttons of gameLevel
 
-    // Creating variable "kerroin" to ...
+    /* Creating variable "kerroin" to multiply a decimal random number.
+    The value will depend on which level of game the user might choose.  */
     let kerroin;
 
-    // Using ".addEventListener" property to ...
+    /* Using the ".addEventListener" method to execute the function 
+       that gives the value of "3" to variable "kerroin" 
+       when clicking on "easy level" button (round button "1" in the webpage).  */
     buttonLevelEasy.addEventListener("click", () => {
-        kerroin = 3; 
+        kerroin = 3; // 3 random pictures will be choosen from 4 alternatives.
     }); 
-    // Using ".addEventListener" property to ...
+    /* Using the ".addEventListener" method to execute the function 
+        that gives the value of "10" to variable "kerroin" 
+        when clicking on "medium level" button (round button "2" in the webpage).  */
     buttonLevelMedium.addEventListener("click", () => {
-        kerroin = 10; 
+        kerroin = 10; // 3 random pictures will be choosen from 10 alternatives.
     });
-    // Using ".addEventListener" property to ...
-    buttonLevelHard.addEventListener("click", () => {
-        kerroin = kuvat.length; 
-    }); 
-// END of Adding code for the 3 buttons of gameLevel
 
-// Creating function "valitseKuva" to ...
+    /* Using the ".addEventListener" method to execute the function 
+       that gives the value of "kuvat.length" (21) to variable "kerroin" 
+       when clicking on "hard level" button (round button "3" in the webpage).  */
+    buttonLevelHard.addEventListener("click", () => {
+        kerroin = kuvat.length; // 3 random pictures will be choosen from 21 alternatives.
+    }); 
+// END of Adding code for the 3 buttons of gameLevel.
+
+//Creating function "valitseKuva" to decide which picture will be shown.
 function valitseKuva(){
 
-    /* 
-    1.- Using "Math.random" method to create a random number
+/*  1.- Using "Math.random" method to create a random decimal number
     that will be multiplied by the "kerroin" value.
     2.- Using "Math.floor" method to round the 
     decimal number to the lower integer.  */
-  
     let kuvaNumero=Math.floor(Math.random() * kerroin);
-    // Showing the "kerroin" value in the console
+    
+    // Showing the "kerroin" value in the console.
     console.log("kerroin: "+kerroin);
-    // Using "return" method to ...
+    
+    /* Using the "return" statement to specify the value
+       of "kuvaNumero" that will be provided by the function. */
     return kuvaNumero;
 }
 
-// Creating function "laittaKuva" to ...
+/* Creating function "laittaKuva" that use 2 parameters,
+   to show a picture from Array kuvat (created in kaunisvaasa.js file). */
 function laittaaKuva(vuoro,indexLuku){
 
     imagenLaskuri[vuoro].getElementsByTagName("img")[0].src=kuvat[indexLuku]; 
+    /* How it works:
+    1.- Parameter "vuoro" is the index, that tells 
+        which ikkuna will contain the chosen picture.
+    2.- Using ".getElementsByTagName" method to manage 
+        the element of the "ikkuna" which has the tag "img".
+    3.- Using the ".src" property to replace the image in the ikkuna (img_0) 
+        with the new picture "kuvat" of index "indexLuku".    */
 }
-// Creating function "vertailla" to ...
+/* Creating function "vertailla" to choose which message 
+   will be disply in the message box. */
 function vertailla(){
-   // Using ".getElementById" property to ...
+
+    //Using ".getElementById" method to get the "message" element the html document.
    const mensaje = document.getElementById("mensaje");
-   //Condition: If the 3 elements are equals
+
+   // Using If statetment:
+   /* Condition: If the 3 elements (paikka[0], paikka[1], paikka[2]) are equals, 
+      then a "message" is release as an award. */
    if(paikka[0]==paikka[1] && paikka[1]==paikka[2]){
-        // Using ".innerText" property to ...       
+
+        /* Using ".innerText" property to update 
+           the "message" inside the message box "viestilaatikko"  */   
         mensaje.innerText = palkinnot[paikka[0]];
+
         // Showing winner message in the console
         console.log("Voittoviesti: "+paikka[0]);
 
-        // Calling the function "soundVoittaja"
+        // Calling the function "soundVoittaja" from the soundeffect.js file
         soundVoittaja(); 
    }
 }
