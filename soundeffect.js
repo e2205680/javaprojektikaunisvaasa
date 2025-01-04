47d715716ae0ef2ff9bd78cc189a3f62194d1017
e2205680 <ivonne.cuadrosevangelista@gmail.com>
2025-01-04 14:04:49 +0200

    Adding comments parte 3: soundeffect.js file

--- soundeffect.js
+++ soundeffect.js
@@ -1,21 +1,20 @@
-// Writing comments part 1
-/* 
-All function effects are gathered in a .js file
-Using "Audio" property to ...
-Using "play()" method to ...
+/* 1.- All function of sound effects are gathered in the "soundeffect.js" file
+   2.- Using the constructor function "Audio" to create 
+       an html audio element from the link provided.
+   3.- Using the "play()" method to start playing the audio element provided by the link given.
 */
-
 function soundVoittaja(){
     let audioVoittaja = new Audio("https://codeskulptor-demos.commondatastorage.googleapis.com/descent/gotitem.mp3");
-    audioVoittaja.play();     
+    audioVoittaja.play(); 
 }
 
 function painaNappiSound(){
     let audioNappiSound = new Audio("https://codeskulptor-demos.commondatastorage.googleapis.com/pang/pop.mp3");
-    audioNappiSound.play();   
+    audioNappiSound.play();    
 }
 
 function muutaKuvaSound(){
     let audioMuutaKuvaSound = new Audio("https://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/missile.mp3");
-    audioMuutaKuvaSound.play();   
+    audioMuutaKuvaSound.play(); 
 }
+
